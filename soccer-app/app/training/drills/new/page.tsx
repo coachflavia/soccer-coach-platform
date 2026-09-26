93175a5915e682320de31d2a4bb6d328357d3130
coachflavia <coachflaviaguedes@gmail.com>
2026-09-25 23:35:52 -0500

    Organize training library workspaces

--- soccer-app/app/training/drills/new/page.tsx
+++ soccer-app/app/training/drills/new/page.tsx
@@ -1,2 +1,8 @@
 import { LibraryDrillEditor } from "../../../../components/library-drill-editor";
-export default function NewLibraryDrillPage() { return <LibraryDrillEditor/>; }
+import { libraryAreas, type LibraryArea } from "../../../../lib/drill-library-data";
+
+export default async function NewLibraryDrillPage({ searchParams }: { searchParams: Promise<{ area?: string }> }) {
+  const requested = (await searchParams).area;
+  const area: LibraryArea = libraryAreas.includes(requested as LibraryArea) ? requested as LibraryArea : "personal";
+  return <LibraryDrillEditor initialArea={area}/>;
+}
